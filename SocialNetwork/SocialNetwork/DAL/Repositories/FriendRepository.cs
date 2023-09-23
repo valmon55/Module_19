95f65218d6bf604af3113f60084ad3196f20ee20
valmon55 <fedor_ka@mail.ru>
2023-09-23 14:36:16 +0300

    +db

--- SocialNetwork/SocialNetwork/DAL/Repositories/FriendRepository.cs
+++ SocialNetwork/SocialNetwork/DAL/Repositories/FriendRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialNetwork.DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,21 @@ using System.Threading.Tasks;
 
 namespace SocialNetwork.DAL.Repositories
 {
-    internal class FriendRepository
+    public class FriendRepository : BaseRepository, IFriendRepository
     {
+        public IEnumerable<FriendEntity> FindAllByUserId(int userId)
+        {
+            return Query<FriendEntity>(@"select * from friends where user_id = :user_id", new { user_id = userId });
+        }
+
+        public int Create(FriendEntity friendEntity)
+        {
+            return Execute(@"insert into friends (user_id,friend_id) values (:user_id,:friend_id)", friendEntity);
+        }
+
+        public int Delete(int id)
+        {
+            return Execute(@"delete from friends where id = :id_p", new { id_p = id });
+        }
     }
 }
