03a0669decd5beacbfd694bcb7eda781c498f068
valmon55 <fedor_ka@mail.ru>
2023-09-27 14:26:08 +0300

    + регистрация

--- SocialNetwork/SocialNetwork/Program.cs
+++ SocialNetwork/SocialNetwork/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace SocialNetwork
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+
+namespace SocialNetwork
 {
     internal class Program
     {
+        public static UserService userService = new UserService();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Добро пожаловать в социальную сеть!");
+            Console.WriteLine("Для регистрации пользователя введите имя пользователя:");
+
+            while (true)
+            {
+                string firstName = Console.ReadLine();
+                Console.Write("фамилия:");
+                string lastName = Console.ReadLine();
+                Console.Write("пароль:");
+                string password = Console.ReadLine();
+                Console.Write("почтовый адрес:");
+                string email = Console.ReadLine();
+
+                UserRegistratiionData userRegistratiionData = new UserRegistratiionData()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Password = password,
+                    Email = email
+                };
+                try
+                {
+                    userService.Register(userRegistratiionData);
+                    Console.WriteLine("Регистрация прошла успешно.");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("Введите корректое значение!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Произошла ошибка при регистрации!");
+                }
+
+
+                Console.ReadKey();
+            }
         }
     }
 }
