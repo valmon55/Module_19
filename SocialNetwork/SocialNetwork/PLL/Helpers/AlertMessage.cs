356d60421c62ececc70fe78fd0cbec6602c4f85d
valmon55 <fedor_ka@mail.ru>
2023-09-27 21:23:48 +0300

    +

--- SocialNetwork/SocialNetwork/PLL/Helpers/AlertMessage.cs
+++ SocialNetwork/SocialNetwork/PLL/Helpers/AlertMessage.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SocialNetwork.PLL.Helpers
 {
-    internal class AlertMessage
+    public class AlertMessage
     {
+        public static void Show(string message)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = originalColor;
+        }
     }
 }
--- SocialNetwork/SocialNetwork/PLL/Helpers/SuccessMessage.cs
+++ SocialNetwork/SocialNetwork/PLL/Helpers/SuccessMessage.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace SocialNetwork.PLL.Helpers
 {
-    internal class SuccessMessage
+    public class SuccessMessage
     {
+        public static void Show(string message)
+        {
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = originalColor;
+        }
     }
 }
