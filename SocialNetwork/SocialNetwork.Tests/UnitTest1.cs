190fa9c0b90a4e76f56520e2fc1233170dd4c3b3
valmon55 <fedor_ka@mail.ru>
2023-10-05 12:21:30 +0300

    подготовка тестов

---
+++ SocialNetwork/SocialNetwork.Tests/UnitTest1.cs
@@ -0,0 +1,37 @@
+namespace SocialNetwork.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [Test]
+        public void AddUser()
+        {
+
+        }
+        [Test]
+        public void AddFriend()
+        {
+
+        }
+        [Test]
+        public void RemoveFriend()
+        {
+
+        }
+        [Test]
+        public void FriendList()
+        {
+
+        }
+
+    }
+}
